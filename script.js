376b7657afafa6f8f85fe593a8b850f960efc6e9
bigazzi96 <fbigazzi96@gmail.com>
2024-05-12 17:33:15 +0200

    added css part

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-document.querySelector('#ticket').addEventListener('click',function(){
+const distanceElement = document.querySelector('input#user-distance');
+const ageElement = document.querySelector('input#user-age');
 
+const discountPercentageEl = document.querySelector('span#discount-percentage');
+const priceDiscountEl = document.querySelector('span#price-discount');
+const totalPriceEl = document.querySelector('span#total-price');
+
+document.querySelector('#ticket-generator').addEventListener('click', function(){
+    const distance = Number.parseFloat(distanceElement.value, 10);
+    const age = Number.parseInt(ageElement.value, 10);
+
+    if ( isNaN(distance) || isNaN(age)){
+        console.error('Given inputs are not numbers');
+    } else {
+        const pricePerUnit = 0.267113;
+        let discount = 0;
     
-const distance = Number.parseFloat(document.querySelector('input#user-distance').value,10);
-const age = Number.parseInt(document.querySelector('input#user-age').value,10);
-
-
-if (age > 63) {
-   sconto = 37.893;
-}
-
-else if (age < 21){
-    sconto = 24.552;
-}
-
-
-
+        if ( age >= 63 ) {
+            discount = 37.893
+        } else if ( age < 21){
+            discount = 24.552;
+        }
+    
+        let totalDiscount = (pricePerUnit * distance) / 100 * discount;
+        let total = (pricePerUnit * distance) - totalDiscount;
+    
+        discountPercentageEl.innerHTML = discount + '%';
+        priceDiscountEl.innerHTML = totalDiscount.toFixed(2) + '$';
+        totalPriceEl.innerHTML = total.toFixed(2) + '$';
+    
+        console.log(total, totalDiscount);
+    }
+});
 
 
